5d00ebe1736166207cde1d7f935442b70daa8286
Lucas Alfeu <103508524+LucasAlfeu@users.noreply.github.com>
2022-11-30 09:19:13 -0300

    mudando as rotas do perfil

--- src/Routes/perfilRoutes.js
+++ src/Routes/perfilRoutes.js
@@ -4,8 +4,8 @@ import PerfilController from '../Controllers/perfilController.js';
 const router = express.Router()
 
 router
-    .get("/perfil", PerfilController.listarPerfil)
-    .post("/perfil", PerfilController.cadastrarPerfil)
+    .get("/", PerfilController.listarPerfil)
+    .post("/", PerfilController.cadastrarPerfil)
     .put("/perfil/:id", PerfilController.alterarPerfil)
     .delete("perfil/:id", PerfilController.deletarPerfil)
 
